864694d72282d4e87e2d375b50e46705e123ceb4
Vaddix <megaman20xx@gmail.com>
2016-06-07 17:03:42 -0500

    Forgot to actually add the js and css to the git repo... doing that now ._.

---
+++ js/app.js
@@ -0,0 +1,61 @@
+(function () {
+	var app = angular.module('penguindrum', []);
+
+	app.controller('pinguController', function(){
+		this.family = pengys;
+	});
+
+	app.controller('tabController', function(){
+		this.tab = 1;
+		this.setTab = function(newTab){
+			this.tab=newTab;
+		};
+		this.isTab = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
+	var pengys = [
+		{
+			name: "1",
+			description: "This pengy wears the mask of the playboy. Hiding the memory of all he has given up, he suppresses his true desires and dedicates himself to protecting his 'family'.",
+			images: [
+				"../images/pengy1.jpg",
+				"../images/pengy1.png"
+			]
+		},
+		{
+			name: "2",
+			description: "This pengy wears the mask of the nice guy. Hiding the memory of all his past regrets, he suppresses his fear and anger and dedicates himself to maintaining  his 'family'.",
+			images: [
+				"../images/pengy2.jpg",
+				"../images/pengy2.png"
+			]
+		},
+		{
+			name: "3",
+			description: "This pengy wears the mask of the happy cripple. Hiding the memory of all that has been taken from her, she suppresses her sadness and dedicates herself to supporting her 'family'.",
+			images: [
+				"../images/pengy3.jpg",
+				"../images/pengy3.png"
+			]
+		}
+	];
+	
+	var tabSettings = [
+		{
+			title: 'Tab 1',
+			description: "Tab 1 description goes here!"
+		},
+		{
+			title: 'Tab 2',
+			description: "Tab 2 description goes here!"
+		},
+		{
+			title: 'Tab 3',
+			description: "Tab 3 description goes here!"
+		}
+	];
+
+
+})();
